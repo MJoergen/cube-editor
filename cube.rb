b289b8fab2752e4fd351927e24ad9e683508da74
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-04-18 11:35:00 +0200

    WIP.

---
+++ cube.rb
@@ -0,0 +1,304 @@
+require_relative 'face.rb'
+
+class Cube
+   def initialize(window)
+      @window   = window
+      @image    = Gosu::Image.new('square.png')
+      @font     = Gosu::Font.new(48)
+      @pixels   = 25
+      @size     = 4
+      @max      = @size-1
+      @margin   = 10
+      @xpos     = 200
+      @ypos     = 200
+
+      # Initialize positions on screen of the six faces
+      #
+      #     1
+      #   2 0 3 5
+      #     4
+      #
+      @positions = []
+      @positions << {x:@xpos,                            y:@ypos}
+      @positions << {x:@xpos,                            y:@ypos - (@size*@pixels + @margin)}
+      @positions << {x:@xpos - (@size*@pixels + @margin),   y:@ypos}
+      @positions << {x:@xpos + (@size*@pixels + @margin),   y:@ypos}
+      @positions << {x:@xpos,                            y:@ypos + (@size*@pixels + @margin)}
+      @positions << {x:@xpos + 2*(@size*@pixels + @margin), y:@ypos}
+
+      @faces = []
+      for i in 0..5
+         @faces << Face.new(window, @size, i)
+      end
+
+      @edges0 = make_edge_list(0)
+      @edges1 = make_edge_list(1)
+
+      # 24 corner pieces (incl rotations)
+      @corners = []
+      @corners << [0,   0,   0,  1,   0, @max,  2, @max,   0]
+      @corners << [0,   0, @max,  2, @max, @max,  4,   0,   0]
+      @corners << [0, @max,   0,  3,   0,   0,  1, @max, @max]
+      @corners << [0, @max, @max,  4, @max,   0,  3,   0, @max]
+      @corners << [1,   0,   0,  5, @max,   0,  2,   0,   0]
+      @corners << [1,   0, @max,  2, @max,   0,  0,   0,   0]
+      @corners << [1, @max,   0,  3, @max,   0,  5,   0,   0]
+      @corners << [1, @max, @max,  0, @max,   0,  3,   0,   0]
+      @corners << [2,   0,   0,  1,   0,   0,  5, @max,   0]
+      @corners << [2,   0, @max,  5, @max, @max,  4,   0, @max]
+      @corners << [2, @max,   0,  0,   0,   0,  1,   0, @max]
+      @corners << [2, @max, @max,  4,   0,   0,  0,   0, @max]
+      @corners << [3,   0,   0,  1, @max, @max,  0, @max,   0]
+      @corners << [3,   0, @max,  0, @max, @max,  4, @max,   0]
+      @corners << [3, @max,   0,  5,   0,   0,  1, @max,   0]
+      @corners << [3, @max, @max,  4, @max, @max,  5,   0, @max]
+      @corners << [4,   0,   0,  0,   0, @max,  2, @max, @max]
+      @corners << [4,   0, @max,  2,   0, @max,  5, @max, @max]
+      @corners << [4, @max,   0,  3,   0, @max,  0, @max, @max]
+      @corners << [4, @max, @max,  5,   0, @max,  3, @max, @max]
+      @corners << [5,   0,   0,  1, @max,   0,  3, @max,   0]
+      @corners << [5,   0, @max,  3, @max, @max,  4, @max, @max]
+      @corners << [5, @max,   0,  2,   0,   0,  1,   0,   0]
+      @corners << [5, @max, @max,  4,   0, @max,  2,   0, @max]
+   end
+
+   def make_edge_list(i)
+      j = @max-i
+      edges = []
+      edges << [0,   0,   j,  2, @max,   j]
+      edges << [0,   i,   0,  1,   i, @max]
+      edges << [0,   j, @max,  4,   j,   0]
+      edges << [0, @max,   i,  3,   0,   i]
+      edges << [1,   0,   j,  2,   j,   0]
+      edges << [1,   i,   0,  5,   j,   0]
+      edges << [1,   j, @max,  0,   j,   0]
+      edges << [1, @max,   i,  3,   j,   0]
+      edges << [2,   0,   j,  5, @max,   j]
+      edges << [2,   i,   0,  1,   0,   i]
+      edges << [2,   j, @max,  4,   0,   i]
+      edges << [2, @max,   i,  0,   0,   i]
+      edges << [3,   0,   j,  0, @max,   j]
+      edges << [3,   i,   0,  1, @max,   j]
+      edges << [3,   j, @max,  4, @max,   j]
+      edges << [3, @max,   i,  5,   0,   i]
+      edges << [4,   0,   j,  2,   i, @max]
+      edges << [4,   i,   0,  0,   i, @max]
+      edges << [4,   j, @max,  5,   i, @max]
+      edges << [4, @max,   i,  3,   i, @max]
+      edges << [5,   0,   j,  3, @max,   j]
+      edges << [5,   i,   0,  1,   j,   0]
+      edges << [5,   j, @max,  4,   i, @max]
+      edges << [5, @max,   i,  2,   0,   i]
+
+      return edges
+   end
+
+   def clear
+      for i in 0..5
+         @faces[i].clear
+      end
+   end
+
+   def full
+      for i in 0..5
+         @faces[i].full
+      end
+   end
+
+   def save(filename)
+      f = File.open(filename, "w")
+      for i in 0..5
+         @faces[i].save(f)
+      end
+      f.close()
+   end
+
+   def load(filename)
+      f = File.open(filename, "r")
+      for i in 0..5
+         @faces[i].load(f)
+      end
+      f.close()
+   end
+
+   def draw
+      for i in 0..5
+         @faces[i].draw(@positions[i])
+      end
+
+      if not legal?
+         @font.draw_text("Illegal", 10, 45, 2, 1.0, 1.0,
+                         Gosu::Color.argb(0xff_ffffff))
+      end
+   end
+
+   def check_edges(edges)
+      errors = 0
+      exp = [0]*36
+      obs = [0]*36
+      for i in 0..23
+         edge = edges[i]
+         col1 = @faces[edge[0]].get_col(edge[1], edge[2])
+         col2 = @faces[edge[3]].get_col(edge[4], edge[5])
+         exp_colour_pair = 6*edge[3] + edge[0]
+         exp[exp_colour_pair] += 1
+         if col1 < 6 and col2 < 6
+            colour_pair = 6*col1 + col2
+            obs[colour_pair] += 1
+         end
+      end
+
+      for i in 0..23
+         edge = edges[i]
+         col1 = @faces[edge[0]].get_col(edge[1], edge[2])
+         col2 = @faces[edge[3]].get_col(edge[4], edge[5])
+         if col1 < 6 and col2 < 6
+            colour_pair = 6*col1 + col2
+            if obs[colour_pair] > exp[colour_pair]
+               @faces[edge[0]].draw_illegal(@positions[edge[0]], edge[1], edge[2])
+               @faces[edge[3]].draw_illegal(@positions[edge[3]], edge[4], edge[5])
+               errors += 1
+            end
+         end
+      end
+
+      return errors
+   end
+
+   def check_corners
+      errors = 0
+      exp = [0]*216
+      obs = [0]*216
+      for c in 0..23
+         corner = @corners[c]
+         col1 = @faces[corner[0]].get_col(corner[1], corner[2])
+         col2 = @faces[corner[3]].get_col(corner[4], corner[5])
+         col3 = @faces[corner[6]].get_col(corner[7], corner[8])
+         exp_colour_pair = 36*corner[0] + 6*corner[3] + corner[6]
+         exp[exp_colour_pair] += 1
+         if col1 < 6 and col2 < 6 and col3 < 6
+            colour_pair = 36*col1 + 6*col2 + col3
+            obs[colour_pair] += 1
+         end
+      end
+
+      for c in 0..23
+         corner = @corners[c]
+         col1 = @faces[corner[0]].get_col(corner[1], corner[2])
+         col2 = @faces[corner[3]].get_col(corner[4], corner[5])
+         col3 = @faces[corner[6]].get_col(corner[7], corner[8])
+         if col1 < 6 and col2 < 6 and col3 < 6
+            colour_pair = 36*col1 + 6*col2 + col3
+            if obs[colour_pair] > exp[colour_pair]
+               @faces[corner[0]].draw_illegal(@positions[corner[0]], corner[1], corner[2])
+               @faces[corner[3]].draw_illegal(@positions[corner[3]], corner[4], corner[5])
+               @faces[corner[6]].draw_illegal(@positions[corner[6]], corner[7], corner[8])
+               errors += 1
+            end
+         end
+      end
+
+      return errors
+   end
+
+   def check_faces
+      errors = 0
+      for i in 0..2
+         for j in 0..3
+            for c in 0..5
+               count = 0
+               for f in 0..5
+                  count += @faces[f].count_colour(c, i, j)
+               end
+               if count > 4
+                  for g in 0..5
+                     @faces[g].draw_illegal_face(@positions[g], i, j, c)
+                  end
+                  errors += 1
+               end
+            end
+         end
+      end
+      return errors
+   end
+
+   def legal?
+      errors  = check_faces
+      errors += check_edges(@edges0)
+      errors += check_edges(@edges1)
+      errors += check_corners
+
+      return (errors == 0)
+   end
+
+   def mouse(x, y, colour)
+      for i in 0..5
+         if x >= @positions[i][:x] and x < @positions[i][:x] + @size*@pixels and
+            y >= @positions[i][:y] and y < @positions[i][:y] + @size*@pixels
+            @faces[i].mouse(x - @positions[i][:x], y - @positions[i][:y], colour)
+         end
+      end
+   end
+
+   def left
+      @faces[1].rotate
+      @faces[4].rotate
+      @faces[4].rotate
+      @faces[4].rotate
+      tmp = @faces[5]
+      @faces[5] = @faces[2]
+      @faces[2] = @faces[0]
+      @faces[0] = @faces[3]
+      @faces[3] = tmp
+   end
+
+   def right
+      @faces[1].rotate
+      @faces[1].rotate
+      @faces[1].rotate
+      @faces[4].rotate
+      tmp = @faces[5]
+      @faces[5] = @faces[3]
+      @faces[3] = @faces[0]
+      @faces[0] = @faces[2]
+      @faces[2] = tmp
+   end
+
+   def up
+      @faces[5].rotate
+      @faces[5].rotate
+
+      @faces[2].rotate
+      @faces[2].rotate
+      @faces[2].rotate
+      @faces[3].rotate
+      tmp = @faces[5]
+      @faces[5] = @faces[1]
+      @faces[1] = @faces[0]
+      @faces[0] = @faces[4]
+      @faces[4] = tmp
+
+      @faces[5].rotate
+      @faces[5].rotate
+   end
+
+   def down
+      @faces[5].rotate
+      @faces[5].rotate
+
+      @faces[2].rotate
+      @faces[3].rotate
+      @faces[3].rotate
+      @faces[3].rotate
+      tmp = @faces[5]
+      @faces[5] = @faces[4]
+      @faces[4] = @faces[0]
+      @faces[0] = @faces[1]
+      @faces[1] = tmp
+
+      @faces[5].rotate
+      @faces[5].rotate
+   end
+
+end
+
---
+++ face.rb
@@ -0,0 +1,137 @@
+class Face
+   def initialize(window, size, j)
+      @window = window
+      @image  = Gosu::Image.new('square.png')
+      @pixels = 25
+      @size   = size
+      @max    = size-1
+      @id     = j
+
+      # Initialize colour placement
+      @pieces = [6]*(@size*@size)
+   end
+
+   def clear
+      @pieces = [6]*(@size*@size)
+   end
+
+   def full
+      @pieces = [@id]*(@size*@size)
+   end
+
+   def save(f)
+      for y in 0..@max
+         s = ""
+         for x in 0..@max
+            s += "#{@pieces[@size*y+x]}"
+         end
+         s += "\n"
+         f.write(s)
+      end
+      f.write("\n")
+   end
+
+   def load(f)
+      for y in 0..@max
+         line = f.gets
+         for x in 0..@max
+            @pieces[@size*y+x] = line[x].to_i
+         end
+      end
+      line = f.gets
+   end
+
+   def draw(pos)
+      for x in 0..@max
+         for y in 0..@max
+            @image.draw(pos[:x] + @pixels*x, pos[:y] + @pixels*y, 0, 
+                        (@pixels-4.0)/225, (@pixels-4.0)/225,
+                        @window.get_col(@pieces[@size*y+x]))
+         end
+      end
+   end
+
+   def draw_line(x1, y1, x2, y2, thickness, col)
+      if x1 < x2
+         @window.draw_quad(x1+thickness, y1,           col,
+                           x1,           y1+thickness, col,
+                           x2,           y2-thickness, col,
+                           x2-thickness, y2,           col)
+      else
+         @window.draw_quad(x1-thickness, y1,           col,
+                           x1,           y1+thickness, col,
+                           x2,           y2-thickness, col,
+                           x2+thickness, y2,           col)
+      end
+   end
+
+   def draw_illegal_face(pos, x, y, c)
+      if @pieces[@size*y+x] == c
+         draw_illegal(pos, x, y)
+      end
+      if @pieces[@size*(@max-x)+y] == c
+         draw_illegal(pos, y, @max-x)
+      end
+      if @pieces[@size*(@max-y)+(@max-x)] == c
+         draw_illegal(pos, @max-x, @max-y)
+      end
+      if @pieces[@size*x+(@max-y)] == c
+         draw_illegal(pos, @max-y, x)
+      end
+   end
+
+   def draw_illegal(pos, x, y)
+      draw_line(pos[:x] + @pixels*x,         pos[:y] + @pixels*y,
+                pos[:x] + @pixels*(x+1) - 4, pos[:y] + @pixels*(y+1) - 4,
+                @pixels/4, @window.error_col)
+      draw_line(pos[:x] + @pixels*(x+1) - 4, pos[:y] + @pixels*y,
+                pos[:x] + @pixels*x,         pos[:y] + @pixels*(y+1) - 4,
+                @pixels/4, @window.error_col)
+   end
+
+   def get_col(x, y)
+      return @pieces[@size*y+x]
+   end
+
+   def count_colour(c, x, y)
+      r = 0
+      if @pieces[@size*y+x] == c
+         r += 1
+      end
+      if @pieces[@size*(@max-x)+y] == c
+         r += 1
+      end
+      if @pieces[@size*(@max-y)+(@max-x)] == c
+         r += 1
+      end
+      if @pieces[@size*x+(@max-y)] == c
+         r += 1
+      end
+
+      return r
+   end
+
+   def mouse(x_rel, y_rel, colour)
+      x = (x_rel / @pixels).floor
+      y = (y_rel / @pixels).floor
+      if @pieces[@size*y+x] < 6
+         @pieces[@size*y+x] = 6
+      else
+         @pieces[@size*y+x] = colour
+      end
+   end
+
+   def rotate
+      for x in 0..(@max/2)
+         for y in 0..(@size/2-1)
+            tmp = @pieces[@size*y+x]
+            @pieces[@size*y+x] = @pieces[@size*(@max-x)+y]
+            @pieces[@size*(@max-x)+y] = @pieces[@size*(@max-y)+(@max-x)]
+            @pieces[@size*(@max-y)+(@max-x)] = @pieces[@size*x+(@max-y)]
+            @pieces[@size*x+(@max-y)] = tmp
+         end
+      end
+   end
+
+end
+
